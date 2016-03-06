7e7aa7007820d91ad82371bef0cbe4b8d1ab6c49
Anton Zujev <anton.zujev@tieto.com>
2016-03-06 20:44:39 +0100

    Conversions done, small cosmetic changes made

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -11,16 +11,26 @@ namespace Converter
     {
         static void Main(string[] args)
         {
+            string[] inArray = { "inch", "inches", "''", "\"", "in", "ins" };
+            string[] cmArray = { "centimeter", "centimeters", "cm", "cms" };
+
             Console.WriteLine("Welcome to Unit Converter 3000.");
             string source;
             while (true)
             {
                 Console.WriteLine("\n===============================\nPlease input a number either in centimeters or inches\nand receive the converted value right away.\nNote that you should input your number with the unit.");
+                Console.Write("> ");
                 source = Console.ReadLine();
 
-                if (source.Length < 1) { break; }
+                if (source.Length < 1 || source == "exit") { break; }
 
-                Regex re = new Regex(@"(\d*)\s*(.*)");
+                if (source == "help" || source == "?")
+                {
+                    Console.WriteLine("\nAccepted symbols for inches are " + string.Join(", ", inArray) + ".\nAccepted symbols for centimeters are " + string.Join(", ", cmArray) + ".\nTo display this message write '?' or 'help'.\nTo exit just press enter or write 'exit'.");
+                    continue;
+                }
+
+                Regex re = new Regex(@"(\d*)[\s\.-]*(.*)");
                 Match m = re.Match(source);
 
                 string digit = "";
@@ -37,9 +47,17 @@ namespace Converter
                     Console.WriteLine("Your input is false.");
                     if (digit.Length < 1) { Console.WriteLine("You haven't sent a number."); }
                     if (units.Length < 1) { Console.WriteLine("You haven't defined a unit."); }
+                    continue;
                 }
 
-                Console.WriteLine(digit + " " + units);
+                Console.WriteLine("-------------------------------\n");
+
+                if (inArray.Any(units.Contains)) {
+                    Console.WriteLine(digit + " in =  " + Math.Round(Convert.ToDouble(digit) * 2.54, 3) + " cm");
+                } else if (cmArray.Any(units.Contains))
+                {
+                    Console.WriteLine(digit + " cm =  " + Math.Round(Convert.ToDouble(digit) * 0.393701, 3) + " in");
+                }
             }
         }
     }
